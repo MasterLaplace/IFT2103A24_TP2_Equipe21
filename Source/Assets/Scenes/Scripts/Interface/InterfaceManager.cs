5c5cb2ce8c8a6136f18eb232bf78e81200b1a62d
Marrbol <martin.boucault@epitech.eu>
2024-11-23 05:54:22 +0100

    feat: Implement player management and input handling for multiplayer setup

--- Source/Assets/Scenes/Scripts/Interface/InterfaceManager.cs
+++ Source/Assets/Scenes/Scripts/Interface/InterfaceManager.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class InterfaceManager : MonoBehaviour
 {
     public static InterfaceManager Instance; // Instance globale
 
     public int numberOfPlayers; // Nombre de joueurs sélectionnés
 
+    public List<PlayerControls> playerControls = new List<PlayerControls>();
     private void Awake()
     {
         if (Instance == null)
